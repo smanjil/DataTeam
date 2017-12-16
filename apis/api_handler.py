cc777d09de2e43b44c53e8319395f110f27a2766
smanjil <shresthamanjil21@gmail.com>
2017-12-16 10:10:07 +0545

    api phase completed

--- apis/api_handler.py
+++ apis/api_handler.py
@@ -10,12 +10,30 @@ class DetailedInfoResource(object):
         param = req.url.split('/')[-2]
 
         try:
-            doc = df[(df[param] == val)][['AGENCY_ID', 'MONTHS', 'PL_START_YEAR', 'STATE_ABBR']].head(2).to_dict('list')
+            doc = df[(df[param] == val)][['AGENCY_ID', 'MONTHS', param, 'STATE_ABBR']].to_dict('list')
         except:
-            doc = df[(df[param] == float(val))][['AGENCY_ID', 'MONTHS', 'PL_START_YEAR', 'STATE_ABBR']].head(2).to_dict('list')
+            doc = df[(df[param] == float(val))][['AGENCY_ID', 'MONTHS', param, 'STATE_ABBR']].to_dict('list')
 
         # create json representation
         resp.body = json.dumps(doc, ensure_ascii=False)
 
         # not required as default status returned by framework in 200
         resp.status = falcon.HTTP_200
+
+class ReportResource(object):
+    def on_get(self, req, resp, start_year, end_year):
+        param1 = req.url.split('/')[4]
+        param2 = req.url.split('/')[6]
+
+        try:
+            doc = df[(df[param1] == start_year) & (df[param2] == end_year)][['AGENCY_ID', 'PRIMARY_AGENCY_ID', 'PROD_ABBR', 'PROD_LINE', 'NB_WRTN_PREM_AMT', 'WRTN_PREM_AMT', 'PREV_WRTN_PREM_AMT', 'PRD_ERND_PREM_AMT']].to_dict('list')
+        except:
+            doc = df[(df[param1] == float(start_year)) & (df[param2] == float(end_year))][['AGENCY_ID', 'PRIMARY_AGENCY_ID', 'PROD_ABBR', 'PROD_LINE', 'NB_WRTN_PREM_AMT', 'WRTN_PREM_AMT', 'PREV_WRTN_PREM_AMT', 'PRD_ERND_PREM_AMT']].to_dict('list')
+
+        # create json representation
+        resp.body = json.dumps(doc, ensure_ascii=False)
+
+        # not required as default status returned by framework in 200
+        resp.status = falcon.HTTP_200
+
+        pd.DataFrame(doc).to_csv('reports/' + param1.split('_')[0] + '-report.csv', index=False)
