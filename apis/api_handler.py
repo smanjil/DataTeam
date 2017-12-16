0c8e2133e37c65bbf3dfc0d7bb3be37680ffe692
smanjil <shresthamanjil21@gmail.com>
2017-12-16 09:18:18 +0545

    new requirements

--- apis/api_handler.py
+++ apis/api_handler.py
@@ -3,12 +3,16 @@ import pandas as pd
 import json
 import falcon
 
-df = pd.read_csv('look/finalapi.csv').fillna('')
+df = pd.read_csv('apis/finalapi.csv').fillna('')
 
 class DetailedInfoResource(object):
     def on_get(self, req, resp, val):
         param = req.url.split('/')[-2]
-        doc = df[(df[param] == float(val))][['AGENCY_ID', 'PL_START_YEAR']].head(1).to_dict('list')
+
+        try:
+            doc = df[(df[param] == val)][['AGENCY_ID', 'MONTHS', 'PL_START_YEAR', 'STATE_ABBR']].head(2).to_dict('list')
+        except:
+            doc = df[(df[param] == float(val))][['AGENCY_ID', 'MONTHS', 'PL_START_YEAR', 'STATE_ABBR']].head(2).to_dict('list')
 
         # create json representation
         resp.body = json.dumps(doc, ensure_ascii=False)
--- apis/app.py
+++ apis/app.py
@@ -8,5 +8,14 @@ api = application = falcon.API()
 
 routes = DetailedInfoResource()
 
+# agency (must be a number - int)
 api.add_route('/routes/AGENCY_ID/{val}', routes)
+
+# month (must be a number - int)
+api.add_route('/routes/MONTHS/{val}', routes)
+
+# year (must be a 4 digit number - int)
 api.add_route('/routes/PL_START_YEAR/{val}', routes)
+
+# state (2 letter upper case word - e.g: IN)
+api.add_route('/routes/STATE_ABBR/{val}', routes)
