43ef3d82da223699e84b37bbc515fc3ef445af40
smanjil <shresthamanjil21@gmail.com>
2017-12-17 19:57:17 +0545

    postgre heroku testing

--- db/conn.py
+++ db/conn.py
@@ -5,5 +5,5 @@ from sqlalchemy import create_engine
 
 # for heroku
 engine = create_engine(
-    'postgresql://xagwigpblkwwea:92ecc654ddcd68cb81210e9e17bb6a8b814e44fc76218e620508f2d10dee5dce@ec2-54-235-244-185.compute-1.amazonaws.com:5432/ddkpiqcv67e0au'
+    'postgresql://xagwigpblkwwea:92ecc654ddcd68cb81210e9e17bb6a8b814e44fc76218e620508f2d10dee5dce@ec2-54-235-244-185.compute-1.amazonaws.com/ddkpiqcv67e0au'
 )
