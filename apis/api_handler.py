1ac295e3c0c524277a3abc681d718eec3dffcf9b
smanjil <shresthamanjil21@gmail.com>
2017-12-18 19:58:44 +0545

    api auth added

--- apis/api_handler.py
+++ apis/api_handler.py
@@ -5,33 +5,46 @@ import falcon
 from db.conn import engine
 
 
+authorized_user = ['testuser1', 'testuser2']
+
+
 class DetailedInfoResource(object):
-    def on_get(self, req, resp, val):
-        param = req.url.split('/')[-2]
+    def on_get(self, req, resp, username, val):
+        if username in authorized_user:
+            param = req.url.split('/')[-2]
 
-        doc = pd.read_sql(
-            'select "AGENCY_ID", "MONTHS", "PL_START_YEAR", "STATE_ABBR" from detailed_table where "{0}" = {1}' .format(param, val), engine
-        ).to_dict('list')
+            doc = pd.read_sql(
+                'select "AGENCY_ID", "MONTHS", "PL_START_YEAR", "STATE_ABBR" from detailed_table where "{0}" = {1}' .format(param, val), engine
+            ).to_dict('list')
 
-        # create json representation
-        resp.body = json.dumps(doc, ensure_ascii=False)
+            # create json representation
+            resp.body = json.dumps(doc, ensure_ascii=False)
 
-        # not required as default status returned by framework in 200
-        resp.status = falcon.HTTP_200
+            # not required as default status returned by framework in 200
+            resp.status = falcon.HTTP_200
+        else:
+            raise falcon.HTTPBadRequest(
+                'Unauthorized user!!!!'
+            )
 
 class ReportResource(object):
-    def on_get(self, req, resp, start_year, end_year):
-        param1 = req.url.split('/')[4]
-        param2 = req.url.split('/')[6]
+    def on_get(self, req, resp, username, start_year, end_year):
+        if username in authorized_user:
+            param1 = req.url.split('/')[5]
+            param2 = req.url.split('/')[7]
 
-        doc = pd.read_sql(
-            'select "AGENCY_ID", "PRIMARY_AGENCY_ID", "PROD_ABBR", "PROD_LINE", "NB_WRTN_PREM_AMT", "WRTN_PREM_AMT", "PREV_WRTN_PREM_AMT", "PRD_ERND_PREM_AMT" from detailed_table where "{0}" = {1} and "{2}" = {3}' .format(param1, start_year, param2, end_year), engine
-        ).to_dict('list')
-        
-        # create json representation
-        resp.body = json.dumps(doc, ensure_ascii = False)
+            doc = pd.read_sql(
+                'select "AGENCY_ID", "PRIMARY_AGENCY_ID", "PROD_ABBR", "PROD_LINE", "NB_WRTN_PREM_AMT", "WRTN_PREM_AMT", "PREV_WRTN_PREM_AMT", "PRD_ERND_PREM_AMT" from detailed_table where "{0}" = {1} and "{2}" = {3}' .format(param1, start_year, param2, end_year), engine
+            ).to_dict('list')
+            
+            # create json representation
+            resp.body = json.dumps(doc, ensure_ascii = False)
 
-        # not required as default status returned by framework in 200
-        resp.status = falcon.HTTP_200
+            # not required as default status returned by framework in 200
+            resp.status = falcon.HTTP_200
 
-        pd.DataFrame(doc).to_csv('reports/' + param1.split('_')[0] + '-report.csv', index=False)
+            pd.DataFrame(doc).to_csv('reports/' + param1.split('_')[0] + '-report.csv', index=False)
+        else:
+            raise falcon.HTTPBadRequest(
+                'Unauthorized user!!!!'
+            )
--- apis/app.py
+++ apis/app.py
@@ -14,29 +14,29 @@ api = application = falcon.API()
 detail_routes = DetailedInfoResource()
 
 # agency (must be a number - int)
-api.add_route('/detail/AGENCY_ID/{val}', detail_routes)
+api.add_route('/{username}/detail/AGENCY_ID/{val}', detail_routes)
 
 # month (must be a number - int)
-api.add_route('/detail/MONTHS/{val}', detail_routes)
+api.add_route('/{username}/detail/MONTHS/{val}', detail_routes)
 
 # year (must be a 4 digit number - int)
-api.add_route('/detail/PL_START_YEAR/{val}', detail_routes)
+api.add_route('/{username}/detail/PL_START_YEAR/{val}', detail_routes)
 
 # state (2 letter upper case word - e.g: IN)
-api.add_route('/detail/STATE_ABBR/{val}', detail_routes)
+api.add_route('/{username}/detail/STATE_ABBR/{val}', detail_routes)
 
 
 # report view
 report_routes = ReportResource()
 
 # PL
-api.add_route('/report/PL_START_YEAR/{start_year}/PL_END_YEAR/{end_year}', report_routes)
+api.add_route('/{username}/report/PL_START_YEAR/{start_year}/PL_END_YEAR/{end_year}', report_routes)
 
 # COMMISIONS
-api.add_route('/report/COMMISIONS_START_YEAR/{start_year}/COMMISIONS_END_YEAR/{end_year}', report_routes)
+api.add_route('/{username}/report/COMMISIONS_START_YEAR/{start_year}/COMMISIONS_END_YEAR/{end_year}', report_routes)
 
 # CL
-api.add_route('/report/CL_START_YEAR/{start_year}/CL_END_YEAR/{end_year}', report_routes)
+api.add_route('/{username}/report/CL_START_YEAR/{start_year}/CL_END_YEAR/{end_year}', report_routes)
 
 # ACTIVITY
-api.add_route('/report/ACTIVITY_NOTES_START_YEAR/{start_year}/ACTIVITY_NOTES_END_YEAR/{end_year}', report_routes)
+api.add_route('/{username}/report/ACTIVITY_NOTES_START_YEAR/{start_year}/ACTIVITY_NOTES_END_YEAR/{end_year}', report_routes)
