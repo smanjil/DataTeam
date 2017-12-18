f1652db7bd75581a4201aa2ecce644927c57d23f
smanjil <shresthamanjil21@gmail.com>
2017-12-18 15:29:23 +0545

    tests updated

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,6 @@
 
+import requests
+import pandas as pd
 import json
 import falcon
 from falcon import testing
@@ -6,24 +8,29 @@ import pytest
 from look.app import api
 
 
+final_api_df = pd.read_csv('input/finalapi.csv')
+
+
 @pytest.fixture
 def client():
     return testing.TestClient(api)
 
 
-def test_list_images(client):
-    doc = {
-        "images": [
-            {
-                "href": "/images/abc.png"
-            }
-        ]
-    }
+def test_detail_info(client):
+    doc = final_api_df[final_api_df['AGENCY_ID'] == 3][["AGENCY_ID", "MONTHS", "PL_START_YEAR", "STATE_ABBR"]].to_dict('list')
 
-    response = client.simulate_get('/images')
-    result_doc = response.content
+    response = requests.get('http://localhost:8000/detail/AGENCY_ID/3')
+    result_doc = json.loads(response.text)
 
-    doc = json.dumps(doc)
+    assert result_doc.keys() == doc.keys()
+    assert str(response.status_code) + ' OK' == falcon.HTTP_OK
 
-    assert result_doc.decode() == doc
-    assert response.status == falcon.HTTP_OK
+
+def test_report(client):
+    doc = final_api_df[(final_api_df['PL_START_YEAR'] == 99999) & (final_api_df['PL_END_YEAR'] == 99999)][["AGENCY_ID", "PRIMARY_AGENCY_ID", "PROD_ABBR", "PROD_LINE", "NB_WRTN_PREM_AMT", "WRTN_PREM_AMT", "PREV_WRTN_PREM_AMT", "PRD_ERND_PREM_AMT"]].to_dict('list')
+
+    response = requests.get('http://localhost:8000/report/PL_START_YEAR/99999/PL_END_YEAR/99999')
+    result_doc = json.loads(response.text)
+
+    assert result_doc.keys() == doc.keys()
+    assert str(response.status_code) + ' OK' == falcon.HTTP_OK
